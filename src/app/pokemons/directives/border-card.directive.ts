b7fc8948c9e0d4f374fab4ccc432d3979cc72a4a
Nehuuln <maximeye1234@gmail.com>
2025-11-14 17:28:55 +0100

    fix

--- src/app/pokemons/directives/border-card.directive.ts
+++ src/app/pokemons/directives/border-card.directive.ts
@@ -1,20 +1,30 @@
-import { Directive, ElementRef, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostListener  } from "@angular/core";
 
 @Directive({
   selector: '[pkmnBorderCard]',
   standalone: true
 })
-export class BorderCardDirective {
+export class BorderCardDirective{
+  private defaultHeight: number = 200;
+  
 
-  constructor(private el: ElementRef) {
+  constructor(private el: ElementRef){
+    this.setHeight(this.defaultHeight);
     this.el.nativeElement.style.transition = 'transform 0.2s ease-in-out';
   }
   //quand je survol l'élément
-  @HostListener('mouseenter') onMouseEnter() {
+  @HostListener('mouseenter') onMouseEnter(){
     this.el.nativeElement.style.transform = 'scale(1.05)';
   }
   //quand je pars de l'élément
-  @HostListener('mouseleave') onMouseLeave() {
-    this.el.nativeElement.style.transform = 'scale(1)';
+  @HostListener('mouseleave') onMouseLeave(){
+     this.el.nativeElement.style.transform = 'scale(1)';
+  }
+  private setBorder(color:string){
+    let border = 'solid 4px'+ color;
+    this.el.nativeElement.style.border = border;
+  }
+  private setHeight(height: number){
+    this.el.nativeElement.style.height = height+'px';
   }
 }
